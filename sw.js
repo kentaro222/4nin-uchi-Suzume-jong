ed5f5e60ce519ed5556e9c736b1c1d7906ca7e29
kentaro222 <magdracky@gmail.com>
2026-04-08 12:01:05 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+ 
+const CACHE_NAME = 'suzume-jan-v1';
+
+// キャッシュするファイルのリスト
+const urlsToCache = [
+  './',
+  './index.html',
+  './tiles.png',
+  './manifest.json',
+  './sw.js'
+];
+
+// インストール時にファイルをキャッシュ
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// ネットワークリクエスト時にキャッシュを返す（オフライン対応）
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // キャッシュに存在する場合はそれを返す
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
